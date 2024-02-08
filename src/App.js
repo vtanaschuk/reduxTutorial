e9e10294466c454df185a34026242303b6dd5a12
vasya <vtanaschuk@gmail.com>
2024-02-08 17:11:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { PostsList } from './features/posts/PostsList'
+
+import { Navbar } from './app/Navbar'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <>
+                <PostsList />
+              </>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
